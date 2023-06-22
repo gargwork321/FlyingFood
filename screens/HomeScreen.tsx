0943ba831f98174cead1d4a0a8447d00b032c6b8
Vivek  Garg <vivekgarg@Viveks-MacBook-Air.local>
2023-06-23 00:42:01 +0530

    Home created with header

---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,60 @@
+import { Image, ScrollView, Text, TextInput, View } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import {
+  UserIcon,
+  ChevronDownIcon,
+  MagnifyingGlassIcon,
+  AdjustmentsVerticalIcon,
+} from "react-native-heroicons/outline";
+import Categories from "./components/categories";
+
+const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+  return (
+    <SafeAreaView className="bg-white pt-5">
+      {/* <Text className="text-red-800">HomeScreen</Text> */}
+      <View className="flex-row pb-3 items-center mx-4 space-x-2">
+        <Image
+          source={{
+            uri: "https://www.pngkey.com/png/detail/115-1150420_avatar-png-pic-male-avatar-icon-png.png",
+          }}
+          className="h-7 w-7 bg-gray-300 p-4 rounded-full border-2"
+        />
+        <View className="flex-1">
+          <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+          <Text className="font-bold text-xl">
+            Current Locations
+            <ChevronDownIcon size={20} color="#00CCBB" />
+          </Text>
+        </View>
+        <UserIcon size={30} color="#00CCBB" />
+      </View>
+
+      {/* search */}
+      <View className="flex-row items-center space-x-2 pb-2 mx-4">
+        <View className="bg-gray-200 p-3 flex-row flex-1 items-center space-x-2">
+          <MagnifyingGlassIcon size={20} color="gray" />
+          <TextInput placeholder="Restaurants and cuisines" />
+        </View>
+        <AdjustmentsVerticalIcon color="#00CCBB" />
+      </View>
+
+      {/* Body */}
+
+      <ScrollView>
+        {/* Categories */}
+        {/* Featured Rows */}
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
